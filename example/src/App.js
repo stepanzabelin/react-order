9349dce6d910e618b1f3bdbca3b25d4f08783ea6
Stepan Zabelin <stepanzabelin@gmail.com>
2019-05-26 14:01:37 +0300

    start

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'react-order'
+import Order from 'react-order'
 
 export default class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.initialEnum = ["first", "second", "third", "fourth"];
+
+		this.state = {
+			list: this.initialEnum
+		};
+
+	}
+
+	reverse = () => {
+		this.setState({
+			list: this.state.list.reverse()
+		})
+	}
+
   render () {
+
     return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
-      </div>
+			<div>
+				<div>Text "react-order"</div>
+			
+				<div>
+					<button type="button" onClick={this.reverse}>reverse</button>
+				</div>
+				
+				<Order list={this.state.list}>
+					<div orderkey="first">First</div>	
+					<div orderkey="second">Second</div>	
+					<div orderkey="third">Third</div>	
+					<div orderkey="fourth">Fourth</div>	
+				</Order>
+			</div>
     )
   }
 }
+
--- src/index.js
+++ src/index.js
@@ -2,21 +2,49 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 import styles from './styles.css'
+ 
+const Order = (props) => {
+	const { children, list }  = props;
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
+	let keysOrders = {};
 
-  render() {
-    const {
-      text
-    } = this.props
+	if(Array.isArray(list)) {
+		for(let item in list) {
+			keysOrders[list[item].toString()] = item.toString();
+		}
+	} else {
+		for(let item in list) {
+			keysOrders[item.toString()] = parseInt(list[item]) || 0;
+		}
+	}
 
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
-}
+  const newList = React.Children.toArray(children).map(function (child, i) {
+		let props  = {...child.props};
+
+		if(!props.hasOwnProperty('key')) {
+			props.key = props.orderkey || Math.random().toString();
+		}
+ 
+		if(!props.hasOwnProperty('orderkey')) {
+			props.orderkey = i.toString();
+		}
+
+		if(!props.hasOwnProperty('order')) {
+		  props.order = keysOrders.hasOwnProperty(props.orderkey) ? keysOrders[props.orderkey] : i;
+		}
+
+		return React.cloneElement(child, props);
+
+	}).sort((a, b) => {
+    return (a.props.order - b.props.order);
+  })
+ 
+  return (
+		<React.Fragment>
+			{newList}
+		</React.Fragment>
+	)
+};
+
+
+export default Order;
